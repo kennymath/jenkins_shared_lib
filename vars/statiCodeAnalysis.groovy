7914076b316af92801104dc104e8f38bab81aec8
kennymath <kennymath@example.com>
2023-04-19 19:37:02 -0400

    change the credential for sonarqube analysis

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: 'credentialsId') {
+    withSonarQubeEnv(credentialsId: 'sonarqube-api') {
          sh 'mvn clean package sonar:sonar'
     }
 }
