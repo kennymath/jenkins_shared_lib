7cfd7f6e175f656bdd7d5e9e0d248aad3273b1b7
kennymath <kennymath@example.com>
2023-04-18 22:20:25 -0400

    update the token

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
-    withSonarQubeEnv(credentialsId: 'sonarqube-id') {
+    withSonarQubeEnv(credentialsId: 'credentialsId') {
          sh 'mvn clean package sonar:sonar'
     }
 }
